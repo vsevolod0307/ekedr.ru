4012b4976b9d101c9b8f40d7f86f739e32509d44
Vsevolod <sewa-1997@mail.ru>
2023-05-29 18:55:48 +0700

    adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
-console.log("script init")
+const hamburger = document.querySelector(".header-hamburger");
+const menu = document.querySelector(".header-mobile");
+const closeMenu = document.querySelector(".header-mobile-close");
+
+hamburger.addEventListener("click", () => {
+  menu.style.right = "0";
+  menu.style.transition = "1s all"
+})
+
+closeMenu.addEventListener("click", () => {
+  menu.style.right = "-500px";
+  menu.style.transition = ".5s all"
+})
