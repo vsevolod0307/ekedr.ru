f49daf23be8fdc20193048c272b76b02bdf61d20
Vsevolod <seven@acceptsib.ru>
2023-06-02 14:04:20 +0700

    fixes

--- src/js/script.js
+++ src/js/script.js
@@ -31,22 +31,22 @@ window.addEventListener("DOMContentLoaded", () => {
   
   modalState(modalClose, callPhone, modalWrapper)
   
-  const filter = document.querySelector("#filter");
-  const filterContent = document.querySelector(".filter-mobile-box");
-  const filterMobile = document.querySelector(".filter-mobile");
-  const filterClose = document.querySelector(".filter-mobile-close");
+  // const filter = document.querySelector("#filter");
+  // const filterContent = document.querySelector(".filter-mobile-box");
+  // const filterMobile = document.querySelector(".filter-mobile");
+  // const filterClose = document.querySelector(".filter-mobile-close");
   
-  filter.addEventListener("click", () => {
-    filterContent.style.opacity = "1";
-    filterContent.style.visibility = "visible";
-    filterMobile.style.left = "0";
-    filterMobile.style.transition = "1s all"
-  })
+  // filter.addEventListener("click", () => {
+  //   filterContent.style.opacity = "1";
+  //   filterContent.style.visibility = "visible";
+  //   filterMobile.style.left = "0";
+  //   filterMobile.style.transition = "1s all"
+  // })
   
-  filterClose.addEventListener("click", () => {
-    filterMobile.style.left = "-600px";
-    filterMobile.style.transition = "0.5s all"
-    filterContent.style.opacity = "0";
-    filterContent.style.visibility = "hidden";
-  })
+  // filterClose.addEventListener("click", () => {
+  //   filterMobile.style.left = "-600px";
+  //   filterMobile.style.transition = "0.5s all"
+  //   filterContent.style.opacity = "0";
+  //   filterContent.style.visibility = "hidden";
+  // })
 })
